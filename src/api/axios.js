7f72eb7aae23774bbe8161fcb35936ccc2eadc61
barathraj10 <barathraj.offl@gmail.com>
2026-02-04 17:41:38 +0530

    Typo fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api=axios.create({
-    baseURL:import.meta.env.BASE_URI
+    baseURL:import.meta.env.VITE_BASE_URI
 });
 
 api.interceptors.request.use((config)=>{
